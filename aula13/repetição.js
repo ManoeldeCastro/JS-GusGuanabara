cad520273e96b2f8bb86292dc2eab03a2916e11b
Manoel Dias <115195961+ManoeldeCastro@users.noreply.github.com>
2022-10-25 19:48:13 -0300

    Update repetição.js

--- aula13/repetição.js
+++ aula13/repetição.js
@@ -1,10 +1,24 @@
 var td = "Tudo bem?"
-var i = 1
-while (i <= 6) {
-  console.log(td)
-  i++
-}
+// var i = 1
+// while (i <= 6) {
+//   console.log(td)
+//   i++
+// }
 
-do {
-  console.log(`${i} ${td}`)
-} while (i <= 12)
+// do {
+//   console.log(`${i} ${td}`)
+// } while (i <= 12)
+//codigo acima se refere a laço simples, só pode passar a condição
+// for (i = 1; i <= 6; i++) {
+//   console.log(`${i} ${td}`)
+// }
+
+// var c = 3
+// for (g = 1; g <= 10; g++) {
+//   console.log(`${c} x ${g}: é igual a: ` + c * g)
+// }
+
+var n7 = 7
+for (i = 1; i <= 10; i++) {
+  console.log(`${n7} x ${i}: é igual a: ` + n7 * i)
+}
